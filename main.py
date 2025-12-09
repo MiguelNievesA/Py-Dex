bdd0e264969d46682de515799e288dd38d392322
BlueFenix77 <miguelnieved77@gmail.com>
2025-12-09 09:09:27 -0500

    Se añadió los iconos basicos de la pagina de bienvenida, así como el diseño de la aplicación principal y el archivo .qrc que permite estructurar los distintos iconos

---
+++ main.py
@@ -0,0 +1,16 @@
+############################################
+# Py-Dex: A Simple Application to Explore  #
+#          the Pokémon Universe            #
+#                                          #
+############################################
+#                BETA 2.1                  #
+############################################
+
+
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import Qt
+from PyQt6.uic import loadUi
+import sys
+import os
+
